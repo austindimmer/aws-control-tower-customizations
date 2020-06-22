5bdc0b8fee11bcd8f2db10b662467d9d6f859458
Austin Dimmer <austin@effective-computing.com>
2020-06-22 10:22:44 +0200

    added repo duplication script to allow temporary private customizations

---
+++ DuplicatePublicRepo.ps1
@@ -0,0 +1,4 @@
+﻿cd C:\Temp
+git clone --bare https://github.com/austindimmer/aws-control-tower-customizations.git
+cd aws-control-tower-customizations.git
+git push --mirror https://github.com/austindimmer/aws-control-tower-customizations-private.git
